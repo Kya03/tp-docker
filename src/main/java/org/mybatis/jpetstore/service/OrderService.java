530dcf8789d4b641a546ea93ef2a73935619c8c2
Kya03 <160619731+Kya03@users.noreply.github.com>
2026-02-11 13:55:11 +0000

    Update OrderService.java
    Fix: Corrected unused field in Item and generic exception in OrderService :IllegalStateException : Cette exception est beaucoup plus précise ici. Elle indique que l'application est dans un état où elle ne peut pas continuer car une donnée essentielle (la séquence) manque dans la base de données.Impact SonarQube : Cela corrige l'alerte "Replace generic exceptions with specific library exceptions".

--- src/main/java/org/mybatis/jpetstore/service/OrderService.java
+++ src/main/java/org/mybatis/jpetstore/service/OrderService.java
@@ -1,17 +1,17 @@
 /*
- *    Copyright 2010-2022 the original author or authors.
+ * Copyright 2010-2022 the original author or authors.
  *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *       https://www.apache.org/licenses/LICENSE-2.0
+ * https://www.apache.org/licenses/LICENSE-2.0
  *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.mybatis.jpetstore.service;
 
@@ -54,7 +54,7 @@ public class OrderService {
    * Insert order.
    *
    * @param order
-   *          the order
+   * the order
    */
   @Transactional
   public void insertOrder(Order order) {
@@ -80,7 +80,7 @@ public class OrderService {
    * Gets the order.
    *
    * @param orderId
-   *          the order id
+   * the order id
    *
    * @return the order
    */
@@ -102,7 +102,7 @@ public class OrderService {
    * Gets the orders by username.
    *
    * @param username
-   *          the username
+   * the username
    *
    * @return the orders by username
    */
@@ -114,14 +114,15 @@ public class OrderService {
    * Gets the next id.
    *
    * @param name
-   *          the name
+   * the name
    *
    * @return the next id
    */
   public int getNextId(String name) {
     Sequence sequence = sequenceMapper.getSequence(new Sequence(name, -1));
     if (sequence == null) {
-      throw new RuntimeException(
+      // FIX: Remplacement de RuntimeException par IllegalStateException
+      throw new IllegalStateException(
           "Error: A null sequence was returned from the database (could not get next " + name + " sequence).");
     }
     Sequence parameterObject = new Sequence(name, sequence.getNextId() + 1);
